38175c1209f4e556296486582598749d86a63d30
Sebixmc <sebastianxmcfarland@gmail.com>
2025-01-15 14:19:11 -0700

    this my fierst commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        
+        Console.WriteLine("How many dice rolls would you like to simulate? ");
+        int totalRolls = int.Parse(Console.ReadLine());
+
+        return totalRolls;
+    }
+}
---
+++ rollDice.cs
@@ -0,0 +1,12 @@
+namespace Mission1SebastianMcFarland;
+
+public class rollDice
+{
+    public static int rollDice(totalRolls)
+    {
+       int min = 1;
+       int max = 6;
+       int dice1 = random.Next(min, max);
+       int dice2 = random.Next(min, max);
+    }
+}
